96570289192278885133d1eca5e5c224ed7b45ce
Jeffrey Massung <massung@gmail.com>
2019-08-09 09:21:40 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# My ~/.emacs.d Setup
+
+This is a safe place for me to keep my .emacs.d folder and .emacs file, so I don’t need to keep trying to use Dropbox or copy/paste it everywhere.
+
+## Setup
+
+Just `git clone` this repository from your home directory, `cd` into it, and then run `setup.cmd` (Windows) or `setup.sh` (Linux/Mac) and it will install all the necessary plugins and put the `.emacs` file where it needs to go.
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/env sh
+
+# download emacs plugins/repositories
+git clone https://github.com/company-mode/company-mode
+git clone https://github.com/magnars/dash.el
+git clone https://github.com/syohex/emacs-git-gutter
+git clone https://github.com/rejeep/f.el
+git clone https://github.com/flycheck/flycheck
+git clone https://github.com/auto-complete/popup-el
+git clone https://github.com/magnars/s.el
+git clone https://github.com/slime/slime
+git clone https://github.com/anwyn/slime-company
+
+# get color themese
+git clone https://github.com/dracula/emacs dracula-theme
+git clone https://github.com/jonathanchu/atom-one-dark-theme
+
+# copy the .emacs file to HOME
+cp .emacs $HOME/.emacs
