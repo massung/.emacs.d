26fb7ef656e5b6f4dce215666f7c7cfd02eb504a
Jeffrey Massung <massung@gmail.com>
2024-03-21 12:30:54 -0500

    Initial commit.

---
+++ init.el
@@ -0,0 +1,82 @@
+(add-to-list 'load-path "~/.emacs.d/p/emacs-git-gutter")
+(add-to-list 'load-path "~/.emacs.d/p/powerline")
+(add-to-list 'load-path "~/.emacs.d/p/slime")
+(add-to-list 'load-path "~/.emacs.d/p/timu-rouge-theme")
+
+(require 'git-gutter)
+(require 'powerline)
+(require 'slime-autoloads)
+(require 'timu-rouge-theme)
+
+;; theme
+(powerline-default-theme)
+(load-theme 'timu-rouge t)
+
+;; git gutter
+(global-git-gutter-mode +1)
+
+;; git gutter symbols, just use whitespace
+(custom-set-variables
+ '(git-gutter:added-sign " ")
+ '(git-gutter:deleted-sign " ")
+ '(git-gutter:modified-sign " "))
+
+;; git gutter colors
+(set-face-background 'git-gutter:added "green")
+(set-face-background 'git-gutter:modified "purple")
+(set-face-background 'git-gutter:deleted "red")
+
+;; setup slime
+(setq inferior-lisp-program "sbcl")
+
+;; use command key as meta
+(setq mac-command-modifier 'meta)
+
+;; don't show the startup screen
+(setq inhibit-startup-screen t)
+
+;; disable autosave and creation of backup files
+(setq auto-save-default nil)
+(setq backup-inhibited t)
+
+;; use spaces for everything
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+
+;; enable word wrap
+(setq-default truncate-lines nil)
+
+;; window size
+(setq default-frame-alist '((height . 62) (width . 100)))
+
+;; setup whitespace
+(setq whitespace-style '(lines-tail trailing))
+(setq whitespace-line-column 72)
+
+;; font
+(set-frame-font "PT Mono 14" nil t)
+
+;; random options
+(display-line-numbers-mode t)
+(tool-bar-mode -1)
+(global-whitespace-mode t)
+(show-paren-mode t)
+;(tab-bar-mode)
+;(scroll-bar-mode)
+
+;; save hooks
+(add-hook 'before-save-hook (lambda ()
+                              (unless (eq major-mode 'fundamental-mode)
+                                (delete-trailing-whitespace))
+
+                              ;; convert tabs to spaces
+                              (unless indent-tabs-mode
+                                (untabify (point-min) (point-max)))))
+
+;; if indent mode for lisp should be like Lispworks
+(defun setup-lisp-indentation ()
+  (load-library "cl-indent")
+  (put 'if 'common-lisp-indent-function '(nil nil &body)))
+
+(add-hook 'lisp-mode-hook 'setup-lisp-indentation)
+(add-hook 'lisp-interaction-mode-hook 'setup-lisp-indentation)
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+PACKAGES=(
+    "https://github.com/syohex/emacs-git-gutter"
+    "https://github.com/milkypostman/powerline"
+    "https://github.com/slime/slime"
+    "https://gitlab.com/aimebertrand/timu-rouge-theme"
+)
+
+# create the packages subdirectory if needed
+mkdir -p p
+
+# test for each package already existing
+for PKG in "${PACKAGES[@]}"; do
+    name="$(basename $PKG)"
+    dir="p/$name"
+
+    if [ ! -d "$dir" ]; then
+	echo "Cloning $name..."
+	git clone -q "$PKG" "$dir"
+    fi
+done
